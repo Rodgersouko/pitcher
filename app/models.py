86a8ed150889dab59f7b5385035765eb280a1f97
Rodgersouko <gabrielaouko@gmail.com>
2020-10-27 13:05:43 +0300

    Updated app

--- app/models.py
+++ app/models.py
@@ -1,43 +1,76 @@
-# @@ -11,7 +11,8 @@ class User(db.Model,UserMixin):
-#     secure_password = db.Column(db.String(255),nullable = False)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
-#     comment = db.relationship('Comment', backref='user', lazy='dynamic')
+from . import db,login_manager
+from datetime import datetime
+from flask_login import UserMixin,current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(db.Model,UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255),unique = True,nullable = False)
+    email  = db.Column(db.String(255),unique = True,nullable = False)
+    secure_password = db.Column(db.String(255),nullable = False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship('Pitch', backref='users', lazy='dynamic')
 
 
-#     @property
-# @@ -41,6 +42,7 @@ class Pitch(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     title = db.Column(db.String(255))
-#     post = db.Column(db.Text())
-#     comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#     time = db.Column(db.DateTime, default = datetime.utcnow)
-#     category = db.Column(db.String(255), index = True)
-# @@ -49,6 +51,25 @@ def save_p(self):
-#         db.session.add(self)
-#         db.session.commit()
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
 
 
-#     def __repr__(self):
-#         return f'Pitch {self.post}'
-
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#     id = db.Column(db.Integer, primary_key=True)
-#     comment = db.Column(db.Text())
-#     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
-
-#     def save_p(self):
-#         db.session.add(self)
-#         db.session.commit()
-
-#     def __repr__(self):
-#         return f'comment:{self.comment}'
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
 
+    def __repr__(self):
+        return f'User {self.username}'
 
-# @login_manager.user_loader
-# def load_user(user_id):
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    post = db.Column(db.Text())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True)
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Pitch {self.post}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+        return comments
+
+    
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
---
+++ config.py
@@ -0,0 +1,56 @@
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Access@localhost/pitcher'
+    DEBUG = True
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    #MAIL_USERNAME = os.environ.get("moringa")
+    MAIL_PASSWORD = os.environ.get("Access")
+    SENDER_EMAIL = 'gabrielaouko@gmail.com'
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
+
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+    
+    DEBUG = True
+
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
