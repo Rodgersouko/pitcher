913442274636493de3c4bf343e75f5d9a4ce6c22
Rodgersouko <gabrielaouko@gmail.com>
2020-10-29 11:31:42 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,13 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_simplemde import SimpleMDE
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 
 db = SQLAlchemy()
 mail = Mail()
 bootstap = Bootstrap()
+simple = SimpleMDE()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -31,6 +33,7 @@ def create_app(config_name):
     bootstap.init_app(app)
     configure_uploads(app,photos)
     mail.init_app(app)
+    simple.init_app(app)
     
 
 
